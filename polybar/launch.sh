aece8a1be50f89a92231110aa7d39c20292b7045
NEKO <87582918+n3k0girl@users.noreply.github.com>
2022-05-07 00:09:15 +0200

    Add files via upload

---
+++ polybar/launch.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Terminate already running bar instances
+killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
+
+# Launch Polybar, using default config location ~/.config/polybar/config.ini
+polybar bottom &
+
+echo "Polybar launched..."
